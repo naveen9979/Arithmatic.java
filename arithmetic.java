e84ea5142f4d76fd0767d397bee60321fc7e700f
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-06 05:01:41 -0700

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,42 @@
+public class arithmetic {
+    public static void main(String[]args){
+        int x = 100 + 50;
+        int y = x + 250;
+        int z = y + y;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        
+
+    int N = 5;
+    int S = 3;
+    System.out.println(N-S);
+
+    int t = 34;
+    int f = 55;
+    System.out.println(t*f);
+
+  int r = 12;
+  int e = 23;
+  System.out.println(r/e);
+
+    int W = 5;
+    int A = 2;
+    System.out.println(W%A);
+
+    int v = 23;
+    int k = 45;
+    v++;
+    System.out.println(v);
+    ++k;
+    System.out.println(k);
+
+
+    int b = 90;
+    b--;
+    System.out.println(b);
+
+
+    }
+    
+}
